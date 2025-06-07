6c98d151de16137da10c5baa38e580ef8b0ae527
Plamen Stoilkov <sdplamen@gmail.com>
2025-06-07 13:19:51 +0300

    Add host .onrender.com

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY') #'django-insecure-614yz!1=d0)$bt=o$hx+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'RENDER_EXTERNAL_HOSTNAME']
+ALLOWED_HOSTS = ['sdplamen-resume.onrender.com']
 
 
 # Application definition
