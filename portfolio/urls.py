79c98d4bc20bb149c73c9df04e38604aa08f96b6
Plamen Stoilkov <sdplamen@gmail.com>
2025-06-07 12:27:52 +0300

    Fix portfolio/urls.pyu

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('resume.urls')),
+    path('resume/', include('resume.urls')),
 ]
